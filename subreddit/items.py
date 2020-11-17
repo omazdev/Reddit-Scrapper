d69bfa6212de3eaac04cf8cf79476ed2025d494d
omazdev <omazdev@gmail.com>
2020-11-18 00:56:47 +0100

    Webdev spider: create and use items in the spider

--- subreddit/items.py
+++ subreddit/items.py
@@ -1,12 +1,34 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy.item import Item, Field
+from scrapy.loader.processors import TakeFirst
 
 
-class SubredditItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PostItem(Item):
+    post_title = Field()
+    post_link = Field()
+    post_author = Field()
+    post_author_link = Field()
+    post_date = Field()
+    post_comments_nbr = Field()
+    post_comments_link = Field()
+    post_votes_nbr = Field()
+    post_flair = Field()
+    post_content = Field()
+    post_comments = Field()
+
+
+class CommentItem(Item):
+    comment_author = Field(
+        output_processor=TakeFirst(),
+    )
+    comment_author_link = Field(
+        output_processor=TakeFirst(),
+    )
+    comment_score = Field(
+        output_processor=TakeFirst(),
+    )
+    comment_date = Field(
+        output_processor=TakeFirst(),
+    )
+    comment_text = Field(
+        # output_processor=TakeFirst(),
+    )
--- subreddit/spiders/webdev_spider.py
+++ subreddit/spiders/webdev_spider.py
@@ -1,5 +1,6 @@
 import scrapy
-
+from scrapy.loader import ItemLoader
+from ..items import PostItem, CommentItem
 
 class WebDevSpider(scrapy.Spider):
     name = "webdev"
@@ -7,53 +8,53 @@ class WebDevSpider(scrapy.Spider):
     pages_index = 1
     pages_to_scrape = 1
 
+
     def parse(self, response):
         # extract posts title, url, date, username, flairs, nbr of comments & nbr of votes.
-        for post in response.css('.thing.link'):
-            post_item = {
-                'post_title': post.css('.title.may-blank::text').get(),
-                'post_link': post.css('.title.may-blank::attr(href)').get(),
-                'post_author': post.css('a.author.may-blank::text').get(),
-                'post_author_link': post.css('a.author.may-blank::attr(href)').get(),
-                'post_date': post.css('time.live-timestamp::attr(title)').get(),
-                'post_comments_nbr': post.css('a.bylink.comments::text').get(),
-                'post_comments_link': post.css('a.bylink.comments::attr(href)').get(),
-                'post_votes_nbr': post.css('div.score.unvoted::attr(title)').get(),
-                'post_flair': post.css('span.linkflairlabel::attr(title)').get(),
-            }
-            yield response.follow(post_item['post_comments_link'], self.parse_comments, meta={'post_item': post_item})
+        for post in response.css('div#siteTable .thing.link'):
+            loader = ItemLoader(item=PostItem(), selector=post)
+            loader.add_css('post_title', '.title.may-blank::text')
+            loader.add_css('post_link', '.title.may-blank::attr(href)')
+            loader.add_css('post_author', 'a.author.may-blank::text')
+            loader.add_css('post_author_link', 'a.author.may-blank::attr(href)')
+            loader.add_css('post_date', 'time.live-timestamp::attr(title)')
+            loader.add_css('post_comments_nbr', 'a.bylink.comments::text')
+            loader.add_css('post_comments_link', 'a.bylink.comments::attr(href)')
+            loader.add_css('post_votes_nbr', 'div.score.unvoted::attr(title)')
+            loader.add_css('post_flair', 'span.linkflairlabel::attr(title)')
+            post_item = loader.load_item()
+            post_comments_link = post.css('a.bylink.comments::attr(href)').get()
+            
+            yield response.follow(post_comments_link, self.parse_comments, meta={'post_item': post_item})
 
         next_page = response.css('span.next-button a::attr(href)').get()
         if next_page is not None and self.pages_index < self.pages_to_scrape:
             self.pages_index += 1
             yield response.follow(next_page, self.parse)
 
+
     def parse_comments(self, response):
         # for each post extract comments : author, comment, score, date.
         post_item = response.meta['post_item']
-        post_item['comments'] = {}
-        comment_index = 0
+        post_loader = ItemLoader(item=post_item, response=response)
         
         # Get post content
-        post_item['post_content'] = response.css(
-            'div.thing.linkflair-showoff div.usertext-body p').getall()
-
+        if post_loader.add_css('post_content', 'div#siteTable > div.thing div.usertext-body p') is None:
+            post_loader.add_value('post_content', 'None')
+        
         # TODO Get post_content for other forms of post (video, external link, ...)
         
         # Get comments
-        for comment in response.css('div.thing.noncollapsed.comment'):
-            post_item['comments'][comment_index] = dict(
-                comment_author=comment.css('a.author.may-blank::text').get(),
-                comment_author_link=comment.css(
-                    'a.author.may-blank::attr(href)').get(),
-                comment_score=comment.css(
-                    'span.score.unvoted::attr(title)').get(),
-                comment_date=comment.css(
-                    'time.live-timestamp::attr(title)').get(),
-                comment_text=comment.css('div.entry.unvoted')[0].css(
-                    'form.usertext > div.usertext-body p').getall(),
-            )
+        for comment in response.css('div.commentarea div.sitetable.nestedlisting div.thing.noncollapsed.comment > div.entry.unvoted'):
+            comment_loader = ItemLoader(item=CommentItem(), selector=comment)
+            comment_loader.add_css('comment_author', 'p.tagline > a.author.may-blank::text')
+            comment_loader.add_css('comment_author_link', 'p.tagline > a.author.may-blank::attr(href)')
+            comment_loader.add_css('comment_score', 'p.tagline > span.score.unvoted::attr(title)')
+            comment_loader.add_css('comment_date', 'p.tagline > time.live-timestamp::attr(title)')
+            comment_loader.add_css('comment_text', 'form.usertext > div.usertext-body p')
+            
+            comment_item = comment_loader.load_item()
+            
+            post_loader.add_value('post_comments', dict(comment_item))
 
-            comment_index += 1
-
-        yield post_item
+        yield post_loader.load_item()
